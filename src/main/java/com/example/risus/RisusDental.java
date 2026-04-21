d7bf60c40e7d79b121bb4c02900388cd499dd1df
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2026-04-21 18:10:18 +0500

    Initial Spring Boot project setup and ci/cd

---
+++ src/main/java/com/example/risus/RisusDental.java
@@ -0,0 +1,13 @@
+package com.example.risus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisusDental {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisusDental.class, args);
+	}
+
+}
